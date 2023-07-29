c23b1f9067fe4173847d219fc24e851ec9ef020a
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-07-29 23:53:52 +0530

    Time: 103 ms (99.62%), Space: 93.4 MB (9.98%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,49 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//        int minimum =INT_MAX;
+//        int maximum = 0;
+
+//        for(int i =0;i<prices.size();i++){
+//            minimum=min(minimum,prices[i]);
+//            if(prices[i]>minimum){
+//                maximum = max(maximum,prices[i]-minimum);
+//            }
+//        }
+//        return maximum;
+//     }
+// };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minum =INT_MAX;
+        int maxim = 0;
+
+        for(int i =0;i<prices.size();i++){
+            minum=min(minum,prices[i]);
+            if(prices[i]>minum){
+                maxim=max(maxim,prices[i]-minum);
+            }
+        }
+        return maxim;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
