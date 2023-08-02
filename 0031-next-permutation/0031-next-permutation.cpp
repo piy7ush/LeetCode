f4d04f62618cf700470945f1dd4474a1cdc95539
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-02 22:12:34 +0530

    Time: 2 ms (81.77%), Space: 12 MB (92.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,34 +1,93 @@
 // class Solution {
 // public:
 //     void nextPermutation(vector<int>& nums) {
-//         next_permutation(nums.begin(),nums.end());
+//          (nums.begin(),nums.end());
 //     }
 // };
+
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         int idx = -1;
+//         int n = nums.size();
+
+//         for(int i = n-2;i >= 0;i--){
+//             if(nums[i] < nums[i+1]){
+//                 idx = i;
+//                 break;
+//             }
+//         }
+
+//         if(idx == -1){
+//             reverse(nums.begin(), nums.end());
+//             return;
+//         }
+
+//         for(int i = n-1;i >= 0;i--){
+//             if(nums[i] > nums[idx]){
+//                 swap(nums[i], nums[idx]);
+//                 break;
+//             }
+//         }
+//         reverse(nums.begin() + idx + 1, nums.end());
+//     }
+// };
+
 
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx = -1;
+
+        int index = -1;
         int n = nums.size();
 
-        for(int i = n-2;i >= 0;i--){
-            if(nums[i] < nums[i+1]){
-                idx = i;
+        for(int i =n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
                 break;
             }
         }
-
-        if(idx == -1){
-            reverse(nums.begin(), nums.end());
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
             return;
         }
 
-        for(int i = n-1;i >= 0;i--){
-            if(nums[i] > nums[idx]){
-                swap(nums[i], nums[idx]);
-                break;
+        for(int i =n-1;i>=0;i--){
+            if(nums[i]>nums[index]){
+            swap(nums[i],nums[index]);
+             break;
             }
+           
         }
-        reverse(nums.begin() + idx + 1, nums.end());
-    }
+
+        reverse(nums.begin()+index+1,nums.end());
+
+
+
+       
+     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
