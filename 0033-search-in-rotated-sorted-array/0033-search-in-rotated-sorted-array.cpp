790d4414f28ea7b7a402395fa47ad83ea5ac752e
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-04 23:02:34 +0530

    Time: 7 ms (29.87%), Space: 11.1 MB (44.3%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,43 @@
-// class Solution {
-// public:
-//     int search(vector<int>& nums, int target) {
-//         //sort(nums.begin(),nums.end());
-
-//         int low =0; 
-//         int high =nums.size()-1;
-//         while(low<=high){
-//             int mid = (low+high)/2;
-//             if(nums[mid]==target){
-//                 return mid;
-//             }
-//             else if(nums[mid]>target){
-//                 high = mid-1;
-//             }
-//             else{
-//                 low = mid+1;
-//             }
-//         }
-//         return -1;
-//     }
-// };
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]==target){
-                return i;
+        int low =0; 
+        int high =nums.size()-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low]&&target<=nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(target>=nums[mid]&&target<=nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         for(int i =0;i<nums.size();i++){
+//             if(nums[i]==target){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
