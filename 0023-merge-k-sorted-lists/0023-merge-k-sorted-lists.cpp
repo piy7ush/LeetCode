43be6ba6e7f417f3980e0ea93e524450d9a1857d
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-08 00:51:15 +0530

    Time: 21 ms (84.12%), Space: 14 MB (11.38%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        vector<int>ans;
+        for(auto x :lists){
+            while(x!=NULL){
+                ans.push_back(x->val);
+                x = x->next;
+            }
+        }
+        sort(ans.begin(),ans.end());
+        ListNode*head1 = NULL;
+        ListNode*tail = NULL;
+        for(auto XXX: ans){
+            ListNode* n = new ListNode(XXX);
+            if(head1==NULL){
+                head1 = n;
+                tail = n;
+            }
+            else{
+                tail->next = n;
+                tail = n;
+            }
+        }
+        return head1;
+    }
+};
