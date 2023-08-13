b3fd68f8a76b8e567ad7d908b6d926f923944a3c
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-13 10:17:44 +0530

    Time: 18 ms (5.26%), Space: 12.5 MB (5.24%) - LeetHub

---
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        map<int,int>mp;
+        for(auto X:arr){
+            mp[X]++;
+        }
+        for(int i=1;i<=2000;i++){
+            if(mp[i]==0){
+                k--;
+            }
+            if(k==0){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
+
+
+
