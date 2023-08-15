52c096a111fb06d7e7ed4c8f7b01e3997abab905
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-15 21:54:28 +0530

    Time: 369 ms (59.97%), Space: 8.6 MB (76.53%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,24 +1,50 @@
+// class Solution {
+// public:
+//     int beauty(vector<int>&ans){
+//         int mini = INT_MAX;
+//         int maxi = INT_MIN;
+//         for(int i=0;i<26;i++){
+//             maxi = max(maxi,ans[i]);
+//             if(ans[i]>=1){
+//                 mini = min(mini,ans[i]);
+//             }
+//         }
+//         return maxi-mini;
+//     }
+//     int beautySum(string s) {
+//         int result =0;
+//         for(int i=0;i<s.size();i++){
+//             vector<int>ans(26,0);
+//             for(int j=i;j<s.size();j++){
+//                 ans[s[j]-'a']++;
+//                 result =result+beauty(ans);
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
-int beauty(vector<int>&alpha){
-    int mf =INT_MIN;
-    int lf=INT_MAX;
-    for(int i=0;i<26;i++){
-        mf = max(mf,alpha[i]);
-        if(alpha[i]>=1){
-            lf = min(lf,alpha[i]);
+    int beauty(vector<int>&ans){
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        for(int i=0;i<26;i++){
+            maxi = max(maxi,ans[i]);
+            if(ans[i]>=1){
+                mini = min(mini,ans[i]);
+            }
         }
+        return maxi-mini;
     }
-    return mf-lf;
-}
-
     int beautySum(string s) {
         int result =0;
         for(int i=0;i<s.size();i++){
-            vector<int>alpha(26,0);
+            vector<int>ans(26,0);
             for(int j=i;j<s.size();j++){
-                alpha[s[j]-'a']++;
-                result = result+beauty(alpha);
+                ans[s[j]-'a']++;
+                result = result+beauty(ans);
             }
         }
         return result;
