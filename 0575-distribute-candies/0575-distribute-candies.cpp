895123d21950ab734d813ef5d3085375c38d4745
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-17 21:51:51 +0530

    Time: 264 ms (64.83%), Space: 120.2 MB (17.33%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,3 +1,19 @@
+// class Solution {
+// public:
+//     int distributeCandies(vector<int>& c) {
+//         set<int>ans;
+//         for(int i=0;i<c.size();i++){
+//             ans.insert(c[i]);
+//         }
+//         int count =0;
+//         for(int j=0;j<ans.size() && j<c.size()/2;j++){
+//             count++;
+
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int distributeCandies(vector<int>& c) {
@@ -5,11 +21,8 @@ public:
         for(int i=0;i<c.size();i++){
             ans.insert(c[i]);
         }
-        int count =0;
-        for(int j=0;j<ans.size() && j<c.size()/2;j++){
-            count++;
-
-        }
-        return count;
+        if(ans.size()>c.size()/2) return c.size()/2;
+        else return ans.size();
     }
 };
+
