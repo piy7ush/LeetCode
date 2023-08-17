621473665e48f75575edc0903a504ee4f83bb871
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-17 21:28:58 +0530

    Time: 36 ms (76.96%), Space: 25.9 MB (34.25%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.cpp
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+    int findPoisonedDuration(vector<int>& t, int duration) {
         if(duration==0)return 0;
         int ans =0;
-        for(int i=0;i<timeSeries.size()-1;i++){
-            ans+=min(timeSeries[i+1]-timeSeries[i],duration);
-
+        for(int i=0;i<t.size()-1;i++){
+            ans+=min(t[i+1]-t[i],duration);
         }
         return ans+duration;
     }
 };
+
+// class Solution {
+// public:
+//     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+//         set<int>ans;
+//         for(int i=0;i<timeSeries.size();i++){
+//             for(int j=timeSeries[i];j<=timeSeries[i]+duration-1;j++){
+//                 ans.insert(j);
+//             }
+//         }
+//         return ans.size();
+//     }
+// };
