8afe02dfe3fff222a75d7567204bcceda75044a8
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-17 10:28:04 +0530

    Time: 10 ms (79.4%), Space: 8.4 MB (49.3%) - LeetHub

--- 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.cpp
+++ 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>ans;
-
         if(s[0]!='a') return false;
-
         for(int i=0;i<s.size();i++){
             if(s[i]=='a'){
                 ans.push(s[i]);
             }
             else if(s[i]=='b'){
-                if(!ans.empty() &&ans.top()=='a'){
+                if(!ans.empty() && ans.top()=='a'){
                     ans.push(s[i]);
                 }
-                else return false;                
-            }
-
-            else{
-                if(!ans.empty()&&ans.top()=='b'){
-                    ans.pop();
-                 if(!ans.empty() && ans.top()=='a'){
-                    ans.pop();
-                    }
-                    else return false;
-
-                }
-
-                
                 else return false;
             }
+            else{
+                if(!ans.empty() && ans.top()=='b'){
+                    ans.pop();
+                
+                if(!ans.empty() && ans.top()=='a'){
+                    ans.pop();
+                }
+                    else return false;
+                }
+                else return false;
+            }
+           
+            
         }
         return ans.empty();
-
-
     }
 };
