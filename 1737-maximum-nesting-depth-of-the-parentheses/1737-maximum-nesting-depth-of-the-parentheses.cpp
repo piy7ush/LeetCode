527c9fdc315e3a40fda49da688ef6fc1701e5726
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-20 22:41:09 +0530

    Time: 3 ms (39.79%), Space: 6.2 MB (77.01%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,18 +1,34 @@
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         int count =0;
+//         int maxi =0;
+
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]=='('){
+//                 count++;
+//                 maxi = max(count,maxi);
+//             }
+//             else if(s[i]==')'){
+//                 count--;
+//             }
+
+//         }
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int maxDepth(string s) {
         int count =0;
         int maxi =0;
-
         for(int i=0;i<s.size();i++){
             if(s[i]=='('){
                 count++;
                 maxi = max(count,maxi);
             }
-            else if(s[i]==')'){
-                count--;
-            }
-
+            else if(s[i]==')')count--;
         }
         return maxi;
     }
