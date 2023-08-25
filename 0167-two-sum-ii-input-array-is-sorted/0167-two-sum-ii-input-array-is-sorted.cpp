356b405b0642bffed33bdfc22f4db8011bf01c6c
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-25 12:23:42 +0530

    Time: 7 ms (94.16%), Space: 15.7 MB (9.84%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         vector<int>ans;
+
+//         for(int i=0;i<numbers.size();i++){
+//             for(int j=i+1;j<numbers.size();j++){
+//                 if(numbers[i]+numbers[j]==target){
+//                     ans.push_back(i+1);
+//                     ans.push_back(j+1);
+//                 }
+//             }
+//         }
+//         return ans;
+
+
+//     }
+// };
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        // vector<int>ans;
+        for(int i=0, j=numbers.size()-1;i<j;){
+            if(numbers[i]+numbers[j]==target){
+                // ans.push_back(i+1);
+                // ans.push_back(j+1);
+                return{i+1,j+1};
+            }
+            else if(numbers[i]+numbers[j]<target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return {};        
+    }
+};
