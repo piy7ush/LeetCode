c0edf4af3307a655db86a3740c65504d3eed7862
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-08-25 12:31:35 +0530

    Time: 11 ms (69.18%), Space: 15.6 MB (30.39%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -18,23 +18,42 @@
 // };
 
 
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         // vector<int>ans;
+//         for(int i=0, j=numbers.size()-1;i<j;){
+//             if(numbers[i]+numbers[j]==target){
+//                 // ans.push_back(i+1);
+//                 // ans.push_back(j+1);
+//                 return{i+1,j+1};
+//             }
+//             else if(numbers[i]+numbers[j]<target){
+//                 i++;
+//             }
+//             else{
+//                 j--;
+//             }
+//         }
+//         return {};        
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        // vector<int>ans;
-        for(int i=0, j=numbers.size()-1;i<j;){
-            if(numbers[i]+numbers[j]==target){
-                // ans.push_back(i+1);
-                // ans.push_back(j+1);
-                return{i+1,j+1};
-            }
-            else if(numbers[i]+numbers[j]<target){
-                i++;
-            }
-            else{
+        int i = 0;
+        int j = numbers.size()-1;
+        while(i<j)
+        {
+            if(numbers[i]+numbers[j]>target)
                 j--;
-            }
+            else if(numbers[i]+numbers[j]<target)
+                i++;
+            if(numbers[i]+numbers[j]==target)
+                return {i+1,j+1};
         }
-        return {};        
+        return {};
     }
 };
+
