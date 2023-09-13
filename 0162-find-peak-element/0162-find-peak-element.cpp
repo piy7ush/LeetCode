b36f045a2eb48b0250112e8cf1e605c0e0dd2703
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-09-13 15:00:03 +0530

    Time: 6 ms (33.64%), Space: 9.2 MB (19.77%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,42 +1,24 @@
-// class Solution {
-// public:
-//     int findPeakElement(vector<int>& nums) {
-//         int s =0;
-//         int e = nums.size()-1;
-//         int mid = (s+e)/2;
-
-//         while(s<e){
-//             if(nums[mid]<nums[mid+1]){
-//                 s =mid+1;
-
-//             }
-//             else{
-//                 e = mid;
-//             }
-//             mid =(s+e)/2;
-//         }
-//         return mid;
-        
-//     }
-// };
-
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int start = 0;
-        int end =nums.size()-1;
-        int mid = (start+end)/2;
+        // int start = 0;
+        // int end = nums.size()-1;
+        // int mid = (start+end)/2;
 
-        while(start<end){
-            if(nums[mid]<nums[mid+1]){
-                start = mid+1;
-            }
-            else{
-                end = mid;
-            }
-            mid = (start+end)/2;
-
+        // while(start<end){
+        //     if(nums[mid]<nums[mid+1]){
+        //         start = mid+1;
+        //     }
+        //     else{
+        //         end = mid;
+        //     }
+        //     mid = (start+end)/2;
+        // }
+        // return mid;
+        int a = *max_element(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==a)return i;
         }
-        return mid;
+        return 0;
     }
 };
