0fa5f831deb1fa72d15880f811251781cc1329d0
Piyush Yadav <122689583+piy7ush@users.noreply.github.com>
2023-09-17 19:44:08 +0530

    Time: 396 ms (22.09%), Space: 27.1 MB (7.39%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,52 +1,20 @@
-// class Solution {
-// public:
-//     int beauty(vector<int>&ans){
-//         int mini = INT_MAX;
-//         int maxi = INT_MIN;
-//         for(int i=0;i<26;i++){
-//             maxi = max(maxi,ans[i]);
-//             if(ans[i]>=1){
-//                 mini = min(mini,ans[i]);
-//             }
-//         }
-//         return maxi-mini;
-//     }
-//     int beautySum(string s) {
-//         int result =0;
-//         for(int i=0;i<s.size();i++){
-//             vector<int>ans(26,0);
-//             for(int j=i;j<s.size();j++){
-//                 ans[s[j]-'a']++;
-//                 result =result+beauty(ans);
-//             }
-//         }
-//         return result;
-//     }
-// };
-
-
 class Solution {
 public:
-    int beauty(vector<int>&ans){
-        int mini=INT_MAX;
-        int maxi=INT_MIN;
-        for(int i=0;i<26;i++){
-            maxi = max(maxi,ans[i]);
-            if(ans[i]>=1){
-                mini = min(mini,ans[i]);
-            }
-        }
-        return maxi-mini;
-    }
     int beautySum(string s) {
-        int result =0;
+        int ans=0;
         for(int i=0;i<s.size();i++){
-            vector<int>ans(26,0);
+            map<char,int> mp;
             for(int j=i;j<s.size();j++){
-                ans[s[j]-'a']++;
-                result = result+beauty(ans);
+                mp[s[j]]++;
+                int min_ele= INT_MAX;
+                int max_ele= INT_MIN;
+                for(auto x:mp){
+                    max_ele= max(max_ele,x.second);
+                    min_ele= min(min_ele,x.second);
+                }
+                ans+=(max_ele-min_ele);
             }
         }
-        return result;
-    }
+        return ans;
+    } 
 };
